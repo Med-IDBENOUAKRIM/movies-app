bf0f1a7231996ecd7491540a668347802af9535f
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2024-12-23 17:58:57 +0100

    set rate limite for API

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -52,3 +52,8 @@ func (app *Application) editConflictResponse(w http.ResponseWriter, r *http.Requ
 		"unable to update the record due to an edit conflict, please try again"
 	app.errorResponse(w, r, http.StatusConflict, message)
 }
+
+func (app *Application) rateLimitExceededRresponse(w http.ResponseWriter, r *http.Request) {
+	message := "rate limit exceeded"
+	app.errorResponse(w, r, http.StatusTooManyRequests, message)
+}
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *Application) recoverPanic(next http.Handler) http.Handler {
@@ -15,4 +17,18 @@ func (app *Application) recoverPanic(next http.Handler) http.Handler {
 		}()
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (app *Application) rateLimit(next http.Handler) http.Handler {
+	limiter := rate.NewLimiter(2, 4)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			app.rateLimitExceededRresponse()
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+
 }
